0a30af136ce8930fbc0cb9ed0b5dbcd95654083f
Alexander Goncharuk <sashael@ukr.net>
2019-09-15 22:58:20 +0300

    Almost done

--- oopexam/Classes.h
+++ oopexam/Classes.h
@@ -5,11 +5,30 @@
 #include <vector>
 #include <map>
 #include <ctime>
+#include <iomanip>
+#include <conio.h>
+
 using namespace std;
+
+static string Date()
+{
+	string date;
+	time_t t = time(0);
+	tm now;
+	localtime_s(&now, &t);
+	date.append(to_string(now.tm_year + 1900));
+	date.append((to_string(now.tm_mon + 1).size() == 1 ? "0" + to_string(now.tm_mon + 1) : to_string(now.tm_mon + 1)));
+	date.append(to_string(now.tm_mday));
+	date.append(" ");
+	date.append((to_string(now.tm_hour).size() == 1 ? "0" + to_string(now.tm_hour) : to_string(now.tm_hour)));
+	date.append((to_string(now.tm_min).size() == 1 ? "0" + to_string(now.tm_min) : to_string(now.tm_min)));
+	date.append((to_string(now.tm_sec).size() == 1 ? "0" + to_string(now.tm_sec) : to_string(now.tm_sec)));
+	return date;
+}
 
 class Item
 {
-	size_t code;
+	size_t code = 0;
 	string barcode = "0";
 	string name = "";
 	double price = 0;
@@ -50,15 +69,18 @@ public:
 class Receipt abstract
 {
 protected:
-	//string header = "";
 	const size_t number;
 	map<Item*, double> list;
 	double total = 0;
 public:
-	Receipt(/*string header, */size_t number) :number(number)/*, header(header)*/ {}
-	void AddItem(Item* item)
+	Receipt(size_t number) :number(number) {}
+	map<Item*, double>& GetList()
 	{
-		list[item] += 1;
+		return list;
+	}
+	virtual void AddItem(Item* item, double quantity)
+	{
+		list[item] += quantity;
 	}
 	void Show()
 	{
@@ -79,6 +101,7 @@ public:
 	}
 	virtual double Total()
 	{
+		total = 0;
 		for (auto item : list)
 			total += (item.first->GetPrice() * item.second);
 		return total;
@@ -89,33 +112,41 @@ public:
 		ofstream out("Receipts.txt", ios::app);
 		if (out.is_open())
 		{
-			out << number << "\t" << total << endl;
+			out << Date() << "\t" << number << "\t" << total << endl;
 		}
 		else
 			cerr << "Error writing 'Receipts.txt'. Please contact your tech support." << endl;
 		return total;
 	}
+	bool isEmpty()
+	{
+		return list.size() == 0;
+	}
+	size_t GetNumber() const { return number; }
 };
 
 class FiscalR : public Receipt
 {
 public:
-	FiscalR(/*string header, */size_t number):Receipt(/*header, */number){}
+	FiscalR(size_t number) :Receipt(number) {}
 };
 
 class ReturnR : public Receipt
 {
 public:
-	ReturnR(/*string header, */size_t number) :Receipt(/*header, */number) {}
-	double Total()
+	ReturnR(size_t number) :Receipt(number) {}
+	void AddItem(Item* item, double quantity)
 	{
-		for (auto item : list)
-			total -= (item.first->GetPrice() * item.second);
-		return total;
+		list[item] -= quantity;
 	}
+	//double Total()
+	//{
+	//	cout << "ret" << endl;
+	//	for (auto item : list)
+	//		total += (item.first->GetPrice() * item.second);
+	//	return total;
+	//}
 };
-
-//size_t Receipt::number = 1;
 
 class Storage
 {
@@ -125,8 +156,73 @@ private:
 		Load();
 	}
 	static Storage* instance;
-	map<size_t, pair<Item*, int>> items;
+	map<size_t, pair<Item*, double>> items;
+	vector<Item*> items_vector;
 public:
+	void Highlight(bool state)
+	{
+		HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		if (state)
+			SetConsoleTextAttribute(hStdOut, 240);
+		else
+			SetConsoleTextAttribute(hStdOut, 15);
+	}
+	void Print(int highlightLine = -1)
+	{
+		if (items_vector.size() == 0)
+			cout << "Storage is empty." << endl;
+		else
+		{
+			cout << setw(5) << "Code" << setw(15) << "Barcode" << setw(30) << "Name"
+				<< setw(10) << "Price" << setw(7) << "Div" << setw(10) << "Quantity" << endl;
+			for (size_t i = 0; i < items_vector.size(); i++)
+			{
+				if (i == highlightLine)
+					Highlight(1);
+				cout << setw(5) << items_vector[i]->GetCode() << setw(15) << items_vector[i]->GetBarcode()
+					<< setw(30) << items_vector[i]->GetName() << setw(10) << items_vector[i]->GetPrice()
+					<< setw(7) << items_vector[i]->GetDiv() << setw(10) << items[items_vector[i]->GetCode()].second << endl;
+				//cout << endl;
+				//cout << __FUNCTION__ << endl;
+				Highlight(0);
+			}
+		}
+	}
+	Item* GetItem(size_t code)
+	{
+		auto item = items.find(code);
+		if (item == items.end())
+		{
+			cerr << "Error. No such code." << endl;
+			return nullptr;
+		}
+		else
+			return items[code].first;
+	}
+	Item* GetItem(string barcode)
+	{
+		//TODO
+	}
+	Item* GetItem()
+	{
+		int code = -1, value = 0;
+		while (code != 13) {
+			system("cls");
+			code = -1;
+			value = (value + items_vector.size()) % items_vector.size();
+			cout << "Choose item and press 'Enter' button." << endl;
+			Print(value);
+			while (code != 72 && code != 80 && code != 13)
+			{
+				code = _getch();
+				if (code == 72)
+					value--;
+				else if (code == 80)
+					value++;
+			}
+		}
+		return items_vector[value];
+	}
 	static Storage* GetInstance()
 	{
 		if (instance == nullptr)
@@ -156,7 +252,8 @@ public:
 				getline(in, name);
 				in >> price >> dividible >> quantity;
 				Item* tmp = new Item(code, barcode, name, price, dividible);
-				items.insert( { code, { tmp,quantity } } );
+				items_vector.push_back(tmp);
+				items.insert({ code, { tmp,quantity } });
 			}
 		}
 	}
@@ -194,17 +291,24 @@ public:
 		size_t quantity;
 		cout << "Enter code: ";
 		cin >> code;
+		if (items.find(code) != items.end())
+		{
+			cerr << "Error. Code is already used." << endl;
+			return;
+		}
 		cout << "Enter name: ";
-		cin >> name;
+		cin.get();
+		getline(cin, name);
 		cout << "Enter barcode (or 0): ";
 		cin >> barcode;
 		cout << "Enter price: ";
 		cin >> price;
-		cout << "Dividible (true/false): ";
+		cout << "Dividible (1/0): ";
 		cin >> dividible;
 		cout << "Enter quantity: ";
 		cin >> quantity;
 		Item* tmp = new Item(code, barcode, name, price, dividible);
+		items_vector.push_back(tmp);
 		items.insert({ code, {tmp,quantity} });
 	}
 	void DeleteItem(size_t code = 0)
@@ -217,10 +321,26 @@ public:
 		auto del = items.find(code);
 		if (del != items.end())
 		{
+			items_vector.erase(find(items_vector.begin(), items_vector.end(), del->second.first));
 			items.erase(del);
 		}
 		else
 			cerr << "Error. No such code." << endl;
+	}
+	void ParseReceipt(Receipt* rec)
+	{
+		ofstream out("Storage_movement.txt", ios::app);
+		if (out.is_open())
+		{
+			for (auto el : rec->GetList())
+			{
+				items[el.first->GetCode()].second -= el.second;
+				out << Date() << "\t" << rec->GetNumber() << "\t" << -el.second << "\t" << el.first->GetCode() << "\t" 
+					<< el.first->GetName() << "\t" << el.first->GetPrice() << endl;
+			}
+		}
+		else
+			cerr << "Error writing 'Storage_movement.txt'. Please contact your tech support." << endl;
 	}
 	~Storage()
 	{
@@ -240,6 +360,7 @@ private:
 		{
 			getline(in, shopName);
 			in >> lastReceipt;
+			in >> Zreport;
 		}
 		else
 		{
@@ -264,9 +385,12 @@ private:
 	}
 	static Shop* instance;
 	string shopName = "";
-	size_t lastReceipt = 1;
+	size_t lastReceipt = 0;
 	double revenue = 0; //виручка
 	size_t receiptCounter = 0; //к-ть чеків за день
+	size_t Zreport = 0;
+	Receipt* receipt = nullptr;
+	//Storage* storage = Storage::GetInstance();
 public:
 	static Shop* GetInstance()
 	{
@@ -274,7 +398,53 @@ public:
 			instance = new Shop();
 		return instance;
 	}
-
+	void OpenFiscalRec()
+	{
+		if (receipt == nullptr)
+			receipt = new FiscalR(lastReceipt + 1);
+		else
+			cerr << "Error. Close opened receipt first." << endl;
+	}
+	void OpenReturnRec()
+	{
+		if (receipt == nullptr)
+			receipt = new ReturnR(lastReceipt + 1);
+		else
+			cerr << "Error. Close opened receipt first." << endl;
+	}
+	void CloseRec()
+	{
+		if (receipt != nullptr)
+		{
+			if (!receipt->isEmpty())
+			{
+				receipt->Pay();
+				receipt = nullptr;
+				lastReceipt++;
+				receiptCounter++;
+			}
+			else
+				cerr << "Error. Receipt is empty. You can cancel empty receipt." << endl;
+		}
+		else
+			cerr << "Error. No open receipt." << endl;
+	}
+	void CancelRec()
+	{
+		if (receipt != nullptr)
+		{
+			delete receipt;
+			receipt = nullptr;
+		}
+	}
+	Receipt* GetRec()
+	{
+		return receipt;
+	}
+	void XReport()
+	{
+		cout << "Revenue: " << revenue << "\t" << "Number of receipts: " << receiptCounter << endl;
+	}
 };
 
 Shop* Shop::instance = nullptr;
--- oopexam/main.cpp
+++ oopexam/main.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
+#include <windows.h>
 #include "Classes.h"
 using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	Storage* s = Storage::GetInstance();
+	//cout << s->GetItem()->GetName() << endl;
+	//cout << Date() << endl;
 	//s->Load();
 	//s->DeleteItem(2);
 	//s->Save();
-	time_t t = time(0);
+
+	//time_t result = time(NULL);
+	//char str[26];
+	//ctime_s(str, sizeof str, &result);
+	//cout << str << endl;
+
+	/*time_t t = time(0);
 	tm now;
 	localtime_s(&now, &t);
-	cout << (now.tm_year + 1900) << '-' << (now.tm_mon + 1) << '-' << now.tm_mday;
-	Receipt* r = new FiscalR("test", 1);
-	r->Show();
+	cout << (now.tm_year + 1900) << '-' << (to_string(now.tm_mon + 1).size() == 1? "0" + to_string(now.tm_mon + 1):to_string(now.tm_mon + 1)) << '-' << now.tm_mday;*/
+	
+	//time_t t = time(0);
+	//tm now;
+	//localtime_s(&now, &t);
+	//cout << (now.tm_year + 1900) << '-' << (now.tm_mon + 1) << '-' << now.tm_mday;
+
+	//Receipt* r = new FiscalR(1);
+	//Receipt* r2 = new ReturnR(2);
+	//r->Show();
+
+	//cout << s->GetItem(1)->GetName() << endl;
+	//s->AddItem();
+	//s->Save();
+	Shop* shop = Shop::GetInstance();
+	shop->OpenFiscalRec();
+	shop->GetRec()->AddItem(s->GetItem(1), 1);
+	shop->GetRec()->AddItem(s->GetItem(2), 2);
+	shop->GetRec()->AddItem(s->GetItem(2), 0.6);
+	shop->GetRec()->AddItem(s->GetItem(), 3);
+	cout << shop->GetRec()->Total() << endl;
+
+	shop->GetRec()->Show();
+	s->ParseReceipt(shop->GetRec());
+	shop->CloseRec();
 	system("pause");
 	return 0;
 }
